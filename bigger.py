519c7a90c3fcb87fcafc06c2c8fa01fb60758ce1
WeiPing99 <chengweiping@graduate.utm.my>
2022-11-07 23:04:01 -0800

    first commit

---
+++ bigger.py
@@ -0,0 +1,9 @@
+#first_int = 10
+#second_int = 20
+first_int = int(input('The first int: '))
+second_int = int(input('The second int: '))
+if first_int> second_int:
+	print ("The first int is bigger!")
+else:
+	print ("The second int is bigger!")
+
